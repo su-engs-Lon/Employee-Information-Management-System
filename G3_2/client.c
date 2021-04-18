7de6caf3c692e076c4c4c65227967452aa5f63fe
su-engs-Lon <axian666679242460@163.com>
2021-04-18 16:45:52 +0800

    修改内容

--- G3_2/client.c
+++ G3_2/client.c
@@ -1,18 +1,66 @@
 #include "head.h"
 
+
 int do_login(int sockfd, MSG *pbuf)
 {
+	while(1)
+	{
+		puts("please input your name");
+	    gets(buf.name);
+	    puts("please input your passwd");
+	    gets(buf.passwd);
+	    *pbuf.type=LOGIN;
+		
+		send(sockfd, pbuf, sizeof(MSG), 0);
+		recv(clientfd, pbuf,sizeof(MSG), 0);
+		puts(*pbuf.data);
+		
+		if(*pbuf.ret)
+		 
+			continue;
+		else
+			break;
+		
+		
+	}
+	
+	
     
+	return *pbuf.USER_LEVEL;
+	
 }
 
-void do_stuff(int sockfd, MSG *pbuf)
-{
-    
+void do_admin(int sockfd, MSG *pbuf)
+{   while(1)
+    {
+        puts("please input a number represent a common,"
+	                                               "3-ADMIN_ADD_STAFF"   //管理员增加员工
+                                                   "4-ADMIN_DEL_STAFF"    //管理员删除员工
+                                                   "5-ADMIN_QUERY");     //管理员退出
+	    gets(*pbuf.type);
+	    send(sockfd, pbuf, sizeof(MSG), 0);
+		recv(clientfd, pbuf,sizeof(MSG), 0);
+			puts(*pbuf.ret);
+			puts(*pbut.data);		
+		
+	}
+	
+	
 }
 
-int do_admin(int sockfd, MSG *pbuf)
+int do_stuff(int sockfd, MSG *pbuf)
 {
-    
+    puts("please input a number represent a common,1-STAFF_CHANGE_PASSWD"//修改密码
+                                                   " 2-STAFF_QUERY"       //退出
+	                                        "6-SUCCESS" );	
+	while(1)
+	{
+    gets(*pbuf.type);
+	send(sockfd, pbuf, sizeof(MSG), 0);
+	recv(clientfd, pbuf,sizeof(MSG), 0);
+    puts(*pbuf.ret);
+	puts(*pbut.data);
+	}	
 }
 
 int main(int argc, char *argv[])
@@ -20,7 +68,9 @@ int main(int argc, char *argv[])
     int sockfd, ret;
     struct sockaddr_in servaddr;
     MSG buf;
+	int addrlen = sizeof(struct sockaddr);
     char clean[64];
+	pid_t pid;
 
     if (argc < 3)
     {
@@ -41,5 +91,19 @@ int main(int argc, char *argv[])
     servaddr.sin_addr.s_addr = inet_addr(argv[1]);
 
     //连接服务器
+	if(connect(sockfd, (struct sockaddr *)&servaddr, addrlen) < 0){
+        perror("connect err");
+        exit(-1);
+    }
+	
+	USER_LEVEL=do_login(int sockfd, MSG *pbuf);
+	
+	switch(USER_LEVEL){
+		case STAFF:
+		do_stuff(int sockfd, MSG *pbuf);
+		case ADMIN:
+		do_admin(int sockfd, MSG *pbuf);
+		
+	}
 
 }
