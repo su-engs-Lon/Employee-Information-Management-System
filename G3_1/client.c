89c973c1d02101519c450cb9ef81a0990960d8fe
su-engs-Lon <axian666679242460@163.com>
2021-04-19 11:32:18 +0800

    server

--- G3_1/client.c
+++ G3_1/client.c
@@ -2,149 +2,17 @@
 
 int do_login(int sockfd, MSG *pbuf)
 {
-	while(1)
-	{
-    puts("please input your name: ");         //让用户输入用户名和密码
-	fgets(buf.name,N,stdin);
-	puts("please input your passwd\n");
-	fgets(buf.passwd,N,stdin);
-	send(sockfd,pbuf,sizeof(pbuf),0);         //发送用户名密码给服务端，让服务端判断用户名密码是否匹配    
-	nbytes = recv(sockfd, pbuf, sizeof(pbuf), 0);         //接收服务端发送来的ret
-        if(nbytes < 0){
-            perror("recv err");
-            exit(-1);
-        }
-		else if (nbytes > 0)
-		{
-           if(pbuf.ret == 0)                  //判断ret是0 or 1 如果是0,则不匹配；反之，显示登录成功
-		   {
-			   puts("name and passwd does not match");
-			   continue;
-		   }
-		   else if(pbuf.ret == 1)
-		   {
-			   puts(login success!\n);
-			   break;
-		   }
-		} 
-		else
-		{
-            printf("peer exit\n");
-            break;
-        }
-	}
-	
-		
+    
 }
-	
 
 void do_stuff(int sockfd, MSG *pbuf)
 {
-	int option;
-	while(1)
-	{
-	printf("--------you are stuff----------\n");
-	printf("-----1.STAFF_CHANGE_PASSWD-----\n");
-	printf("-----2.STAFF_QUERY        -----\n");
-	printf("-----3.STAFF_EXIT         -----\n");
-	printf("please choose your operating:");
-	scanf("%d",&option);
-	switch(option)
-	{
-		case 1:
-				printf(please input your new passwd:);
-				fgets(pbuf.passwd,N,stdin);
-				pbuf.type = STAFF_CHANGE_PASSWD;
-				send(sockfd,pbuf,sizeof(pbuf),0);
-				recv(sockfd,pbuf,sizeof(pbuf),0);
-				if(pbuf.ret == SUCCESS)
-				{
-					printf("operate success!\n");
-					continue;
-				}
-				else
-				{
-					puts("operate failed!\n");
-					continue;
-				}
-				break;
-		case 2:printf("please input query id:");
-			   scanf("%d",pbuf.info.id);
-			   pbuf.type = STAFF_QUERY;
-			   send(sockfd,pbuf,sizeof(pbuf),0);
-			   recv(sockfd,pbuf,sizeof(pbuf),0);
-			   if(pbuf.ret == SUCCESS)
-			   {
-			   printf("id = %d,name = %s,addr = %s,age = %d,salary = %.1f\n",pbuf.id,pbuf.name,pbuf.addr,pbuf.age,pbuf.salary);
-			   }
-			   else
-			   {
-				   printf("query is failed\n");
-			   }
-			   break;
-		
-		case 3:
-				break;
-				
-	}
-	break;
-	}
+    
 }
 
 int do_admin(int sockfd, MSG *pbuf)
 {
-    int option;
-	while(1)
-	{
-	printf("--------you are admin----------\n");
-	printf("-----   1.ADMIN_QUERY     -----\n");
-/*	printf("-----  2.ADMIN_ADD_STAFF  -----\n");
-	printf("-----  3.ADMIN_DEL_STAFF  -----\n");*/
-	printf("-----  2.STAFF_EXIT       -----\n");
-
-	printf("please choose your operating:");
-	scanf("%d",&option);
-	switch(option)
-	{
-		case 1:
-				printf(please input query id:);
-				fgets(pbuf.INFO.id,N,stdin);
-				pbuf.type = ADMIN_QUERY;
-				send(sockfd,pbuf,sizeof(pbuf),0);
-				recv(sockfd,pbuf,sizeof(pbuf),0);
-				if(pbuf.ret == SUCCESS)
-				{
-					printf("id = %d,name = %s,addr = %s,age = %d,salary = %.1f\n",pbuf.INFO.id,pbuf.INFO.name,pbuf.INFO.addr,pbuf.INFO.age,pbuf.INFO.salary);
-					continue;
-				}
-				else
-				{
-					puts("operate failed!\n");
-					continue;
-				}
-				break;
-/*		case 2:printf("please input query id:");
-			   scanf("%d",pbuf.info.id);
-			   pbuf.type = STAFF_QUERY;
-			   send(sockfd,pbuf,sizeof(pbuf),0);
-			   recv(sockfd,pbuf,sizeof(pbuf),0);
-			   if(pbuf.ret == SUCCESS)
-			   {
-			   printf("id = %d,name = %s,addr = %s,age = %d,salary = %.1f\n",pbuf.id,pbuf.name,pbuf.addr,pbuf.age,pbuf.salary);
-			   }
-			   else
-			   {
-				   printf("query is failed\n");
-			   }
-			   break;
-			   */
-		
-		case 2:
-				break;
-				
-	}
-	break;
-	}
+    
 }
 
 int main(int argc, char *argv[])
@@ -174,15 +42,4 @@ int main(int argc, char *argv[])
 
     //连接服务器
 
-	if(connect(sockfd, (struct sockaddr *)&serveraddr, addrlen) < 0){
-        perror("connect err");
-        exit(-1);
-    }
-	switch(buf.type)
-	{
-		case LOGIN:
-		            do_login(sockfd,buf);
-		case 
-		
-	}
 }
--- G3_2/server.c
+++ G3_2/server.c
@@ -2,24 +2,124 @@
 
 sqlite3 *db = NULL;  //数据库句柄
 
-void do_login(int sockfd, MSG *pbuf)
+void do_login(int socked, MSG *pbuf)
 {
-   
+    char *errmsg;
+    char **resultp;
+    char cmd[256];
+
+    int n_row, n_cloum;
+
+    sprintf(cmd, "select * from usr where name='%s'",pbuf->name);
+
+    puts(cmd);
+
+    if (sqlite3_get_table(db, cmd, &resultp, &n_row, &n_cloum, &errmsg) != SQLITE_OK)
+    {
+        printf("err:%s\n", errmsg);
+        exit(EXIT_FAILURE);
+    }
+
+    if(n_row == 0){
+        strcpy(pbuf->data, "username or password err");
+    }else{
+        strcpy(pbuf->data, "ok");
+    }
+
+    send(socked, pbuf, sizeof(MSG), 0);
 }
 
 void do_staff_change_passwd(int sockfd, MSG *pbuf)
 {
-   
+	char *errmsg;
+	char cmd[256];
+	sprintf(cmd,"update data set passwd = '%s' where name = '%s'",pbuf->passwd,pbuf->name);
+	// 根据 name 修改数据库内容为 passwd
+    if( sqlite3_exec(db,cmd,NULL,NULL,&errmsg) == SQLITE_OK)
+	{
+		//修改成功 ret = 0，data = “修改密码完成”
+		pbuf->ret = -1;
+		strcpy(pbuf->data,"修改密码完成");
+	}
+	else 
+	{
+		//查询失败 ret = -1, data = "修改密码失败“
+		pbuf->ret = 0;
+		strcpy(pbuf->data,"修改密码失败");
+	}
+	send(sockfd,(void *)&pbuf,sizeof(MSG),0);
 }
 
 void do_staff_query(int sockfd, MSG *pbuf)
 {
-    
+    //查找用户信息
+	char *errmsg;
+	char **user_msg;
+	char cmd[256];
+	int  n_row,n_cloum;
+	sprintf(cmd,"select from data where name = %s",pbuf->name);
+
+    if( sqlite3_get_table(db,cmd,&user_msg,&n_row,&n_cloum,&errmsg) == SQLITE_OK)
+	{
+		//查询成功 ret = 0，查询到的信息保存在user_msg中
+		//数据库顺序： name,passwd,id,trueName,addr,age,salary,level 
+		pbuf->ret = -1;
+		strcpy(pbuf->data,"获得员工信息成功");
+		pbuf->info.id = atoi(user_msg[10]);
+		strcpy(pbuf->info.name,user_msg[11]);
+		strcpy(pbuf->info.addr,user_msg[12]);
+		pbuf->info.age = atoi(user_msg[13]);
+		pbuf->info.salary = atof(user_msg[14]);
+		pbuf->ret = atoi(user_msg[15]);
+	}
+	else 
+	{
+		//查询失败 ret = -1, data = "获得员工信息失败“
+		pbuf->ret = 0;
+		strcpy(pbuf->data,"获得员工信息成失败");
+	}
+    //发送 info
+	send(sockfd,(void *)&pbuf,sizeof(MSG),0);
 }
 
 void do_admin_query(int sockfd, MSG *pbuf)
 {
-    
+    // 若 data为空 发送当前用户信息
+	// 若 data不为空 查询用户并将此用户的信息发送
+	if(strcmp(pbuf->data,"") == 0)
+	{
+		do_staff_query(sockfd,pbuf);
+	}
+	else
+	{
+	    char *errmsg;
+	    char sql[64] = {'\0'};
+	    char **user_msg;
+	    int  nrow,ncolumn;
+	    sprintf(sql,"select from data where name = %s",pbuf->data);
+
+        if( sqlite3_get_table(db,sql,&user_msg,&nrow,&ncolumn,&errmsg) != SQLITE_OK)
+  	    {
+		    //查询成功 ret = 0，查询到的信息保存在user_msg中
+		    //数据库顺序： name,passwd,id,trueName,addr,age,salary,level 
+		    pbuf->ret = -1;
+		    strcpy(pbuf->data,"获得员工信息成功");
+		    pbuf->info.id = atoi(user_msg[10]);
+		    strcpy(pbuf->info.name,user_msg[11]);
+		    strcpy(pbuf->info.addr,user_msg[12]);
+		    pbuf->info.age = atoi(user_msg[13]);
+		    pbuf->info.salary = atof(user_msg[14]);
+		    pbuf->ret = atoi(user_msg[15]);
+	    }
+	    else 
+	    {
+		    //查询失败 ret = -1, data = "获得员工信息失败“
+		    pbuf->ret = 0;
+		    strcpy(pbuf->data,"获得员工信息成失败");
+	    }
+        //发送 info
+	    send(sockfd,(void *)&pbuf,sizeof(MSG),0);
+    }
 }
 
 void *handler(void * arg)
@@ -68,10 +168,26 @@ void *handler(void * arg)
     pthread_exit(NULL);
 }
 
+void data_base_init()
+{
+	//初始化数据库
+	char *errmsg;
+    int no;
+    char name[64];
+
+	char sql[256] = {"create table usr(level int,passwd text,id int,name text,addr text \
+					 age int,salary float)"};
+	sqlite3_exec(db,sql,NULL,NULL,&errmsg);
+	sqlite3_exec(db,"insert into data values(101,'bat',1000,'Tony','济南',18,8000);",NULL,NULL,&errmsg);
+	
+}
+
 int main(int argc, char *argv[])
 {
     int listenfd, connfd;
     struct sockaddr_in myaddr;
+	void*ret;
+	pthread_t tid;
 
     if (argc < 3)
     {
@@ -84,7 +200,8 @@ int main(int argc, char *argv[])
         printf("fail to sqlite3_open : %s\n", sqlite3_errmsg(db));
         return -1;
     }
-
+	
+     data_base_init();
     //创建服务器socket
     listenfd = socket(PF_INET, SOCK_STREAM, 0);
     if(listenfd < 0){
@@ -100,9 +217,24 @@ int main(int argc, char *argv[])
         perror("fail to bind");
         exit(-1);
     }
-
-    // XXX int listen(int sockfd, int backlog);
-
-    //接受客户端请求，创建子线程
-
+	// XXX int listen(int sockfd, int backlog);
+	if (listen(listenfd, 5) < 0)
+    {
+        perror("fail to listen");
+        exit(-1);
+    }
+   //接受客户端请求，创建子线程
+    
+        if((connfd = accept(listenfd, NULL, NULL)) < 0){
+            perror("fail to accept");
+            exit(-1);
+        }
+    if(pthread_create(&tid, NULL, handler, &connfd) != 0)
+	{
+		perror("fail to create");
+		exit(1);
+	}
+	//pthread_join(tid,NULL);
+	pthread_join(tid, &ret);
+	
 }
