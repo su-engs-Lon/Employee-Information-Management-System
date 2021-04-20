73b43633c20bd8ae105793571621fff24e3a320d
su-engs-Lon <axian666679242460@163.com>
2021-04-20 18:39:49 +0800

    又改一次

--- G3_2/client.c
+++ G3_2/client.c
@@ -5,6 +5,13 @@ int do_login(int sockfd, MSG *pbuf)
 	while(1)
 	{
 		puts("please input your name");
+<<<<<<< HEAD
+=======
+	    gets(pbuf->name);
+	    puts("please input your passwd");
+	    gets(pbuf->passwd);
+	    pbuf->type=LOGIN;
+>>>>>>> fc9cad3396246ffc08aa7922d0ecdc11da9b3858
 		
 	    scanf("%s",pbuf->info.name);
 	    puts("please input your passwd");
@@ -13,11 +20,15 @@ int do_login(int sockfd, MSG *pbuf)
 	    pbuf->type=LOGIN;
 		printf("发送之前\n");
 		send(sockfd, pbuf, sizeof(MSG), 0);
+<<<<<<< HEAD
 		//send(sockfd, pbuf, sizeof(MSG), 0);
 		printf("发送之后接收之前\n");
 		recv(sockfd, pbuf,sizeof(MSG), 0);
 		//recv(sockfd, pbuf,sizeof(MSG), 0);
 		printf("接收之后\n");
+=======
+		recv(clientfd, pbuf,sizeof(MSG), 0);
+>>>>>>> fc9cad3396246ffc08aa7922d0ecdc11da9b3858
 		puts(pbuf->data);
 		
 		if(pbuf->ret)//ret==1代表操作结果错误
@@ -29,13 +40,18 @@ int do_login(int sockfd, MSG *pbuf)
 			if(c=='y')
 				continue;
 			else
+<<<<<<< HEAD
 				exit(0);
+=======
+				exit 0;
+>>>>>>> fc9cad3396246ffc08aa7922d0ecdc11da9b3858
 		}
 		else
 			break;
 	} 
 	return pbuf->USER_LEVEL;
 }
+<<<<<<< HEAD
 //增加员工的函数
  void do_add_stuff(int sockfd,MSG *pbuf)
  {
@@ -62,35 +78,74 @@ int do_login(int sockfd, MSG *pbuf)
 	 
 	 send(sockfd, pbuf, sizeof(MSG), 0);
 	recv(sockfd, pbuf,sizeof(MSG), 0);
+=======
+//增加员工的函
+ void do_add_stuff(int sockfd,MSG *pbuf)
+ {
+	 puts("please input stuff id");
+	 gets(pbuf->id);
+	 puts("please input stuff name");
+	 gets(pbuf->name[N]);
+	 puts("please input stuff addr");
+	 gets(pbuf->addr[N]);
+	 puts("please input stuff age");
+	 gets(pbuf->age);
+	 puts("please input stuff salary");
+	 gets(pbuf->salary);
+	 
+	 pbuf->type=ADMIN_ADD_STAFF;
+	 
+	 send(sockfd, pbuf, sizeof(MSG), 0);
+	recv(clientfd, pbuf,sizeof(MSG), 0);
+>>>>>>> fc9cad3396246ffc08aa7922d0ecdc11da9b3858
 	puts(pbuf->data);
  }
  //减少员工的函数
  void do_del_stuff(int sockfd,MSG *pbuf)
  {
+<<<<<<< HEAD
 	 puts("please input stuff name");
 	 getchar();
 	 gets(pbuf->info.name);
+=======
+	 puts("please input stuff id");
+	 gets(pbuf->id);
+>>>>>>> fc9cad3396246ffc08aa7922d0ecdc11da9b3858
 	 
 	 pbuf->type=ADMIN_DEL_STAFF;
 	 
 	 send(sockfd, pbuf, sizeof(MSG), 0);
+<<<<<<< HEAD
 	recv(sockfd, pbuf,sizeof(MSG), 0);
+=======
+	recv(clientfd, pbuf,sizeof(MSG), 0);
+>>>>>>> fc9cad3396246ffc08aa7922d0ecdc11da9b3858
 	puts(pbuf->data);
  }
  //管理员查询的函数
  void do_query_admin(int sockfd,MSG *pbuf)
  {
+<<<<<<< HEAD
 	 puts("please input stuff name");
 	 getchar();
 	 gets(pbuf->info.name);
+=======
+	 puts("please input stuff id");
+	 gets(pbuf->id);
+>>>>>>> fc9cad3396246ffc08aa7922d0ecdc11da9b3858
 	 
 	 pbuf->type=ADMIN_QUERY;
 	 
 	 send(sockfd, pbuf, sizeof(MSG), 0);
+<<<<<<< HEAD
 	recv(sockfd, pbuf,sizeof(MSG), 0);
 	puts(pbuf->data);
 	                                                                                                             
 	printf("员工信息为:%d,%s,%s,%d,%6.1f\n",pbuf->info.id,pbuf->info.name,pbuf->info.addr,pbuf->info.age,pbuf->info.salary); 
+=======
+	recv(clientfd, pbuf,sizeof(MSG), 0);
+	puts(pbuf->data);
+>>>>>>> fc9cad3396246ffc08aa7922d0ecdc11da9b3858
  }
 void do_admin(int sockfd, MSG *pbuf)
 {   while(1)
@@ -100,12 +155,17 @@ void do_admin(int sockfd, MSG *pbuf)
         puts("4-ADMIN_DEL_STAFF");                          //管理员删除员工
         puts("5-ADMIN_QUERY");                              //管理员查询
 		puts("6-QUIT");                                      //退出
+<<<<<<< HEAD
 	   // gets(pbuf->type);
 	   scanf("%d",&pbuf->type);
+=======
+	    gets(pbuf->type);
+>>>>>>> fc9cad3396246ffc08aa7922d0ecdc11da9b3858
 		switch(pbuf->type)
 		{
 			case ADMIN_ADD_STAFF :
 			//增加员工的函数
+<<<<<<< HEAD
 			do_add_stuff(sockfd,pbuf);
 			break;
 			case ADMIN_DEL_STAFF :
@@ -119,6 +179,21 @@ void do_admin(int sockfd, MSG *pbuf)
 			case QUIT :
 			close(sockfd);
 			exit (0);
+=======
+			do_add_stuff(sockfd,&pbuf);
+			break;
+			case ADMIN_DEL_STAFF :
+			//减少员工的函数
+			do_del_stuff(sockfd,&pbuf);
+			break;
+			case ADMIN_QUERY ：
+			//管理员查询的函数
+			do_query_admin(sockfd,&pbuf);
+			break;
+			case QUIT :
+			close(sockfd);
+			exit 0;
+>>>>>>> fc9cad3396246ffc08aa7922d0ecdc11da9b3858
 		}	
 		
 	}
@@ -128,6 +203,7 @@ void do_admin(int sockfd, MSG *pbuf)
 //员工改密码的函数
 void staff_change_passwd(int sockfd,MSG *pbuf)
  {
+<<<<<<< HEAD
 	 //puts("please input your name");
 	 //getchar();
 	 //gets(pbuf->info.name);
@@ -138,6 +214,14 @@ void staff_change_passwd(int sockfd,MSG *pbuf)
 	 
 	 send(sockfd, pbuf, sizeof(MSG), 0);
 	recv(sockfd, pbuf,sizeof(MSG), 0);
+=======
+	 puts("please input new password");
+	 gets(pbuf->password);
+	 pbuf->type=STAFF_CHANGE_PASSWD;
+	 
+	 send(sockfd, pbuf, sizeof(MSG), 0);
+	recv(clientfd, pbuf,sizeof(MSG), 0);
+>>>>>>> fc9cad3396246ffc08aa7922d0ecdc11da9b3858
 	puts(pbuf->data);
  }
  //员工查询
@@ -145,9 +229,14 @@ void staff_change_passwd(int sockfd,MSG *pbuf)
  {
 	 pbuf->type=STAFF_QUERY;
 	 send(sockfd, pbuf, sizeof(MSG), 0);
+<<<<<<< HEAD
 	recv(sockfd, pbuf,sizeof(MSG), 0);
 	puts(pbuf->data);
 	printf("员工信息为:%d,%s,%s,%d,%6.1f\n",pbuf->info.id,pbuf->info.name,pbuf->info.addr,pbuf->info.age,pbuf->info.salary); 
+=======
+	recv(clientfd, pbuf,sizeof(MSG), 0);
+	puts(pbuf->data);
+>>>>>>> fc9cad3396246ffc08aa7922d0ecdc11da9b3858
  }
 
 int do_stuff(int sockfd, MSG *pbuf)
@@ -159,6 +248,7 @@ int do_stuff(int sockfd, MSG *pbuf)
     puts("2-STAFF_QUERY");                            //员工查询
 	puts("6-QUIT");                                   //退出  	
 	
+<<<<<<< HEAD
 	//gets(pbuf->type);
 	scanf("%d",&pbuf->type);
 		switch(pbuf->type)
@@ -174,6 +264,22 @@ int do_stuff(int sockfd, MSG *pbuf)
 			case QUIT :
 			close(sockfd);
 			exit (0);
+=======
+	gets(pbuf->type);
+		switch(pbuf->type)
+		{
+			case STAFF_CHANGE_PASSWD ：
+			//员工改密码的函数
+			staff_change_passwd(sockfd,&pbuf);
+			break;
+			case STAFF_QUERY ：
+			//员工查询
+			do_query_stuff(sockfd,&pbuf);
+			break;
+			case QUIT :
+			close(sockfd);
+			exit 0;
+>>>>>>> fc9cad3396246ffc08aa7922d0ecdc11da9b3858
 		}
   }		
 }
@@ -211,6 +317,7 @@ int main(int argc, char *argv[])
         exit(-1);
     }
 	
+<<<<<<< HEAD
 	ret=do_login(sockfd,&buf);
 	
 	switch(ret){
@@ -220,6 +327,17 @@ int main(int argc, char *argv[])
 		
 		case ADMIN://代表管理员
 		do_admin(sockfd, &buf);
+=======
+	ret=do_login(int sockfd, MSG *pbuf);
+	
+	switch(ret){
+		case STAFF://代表普通员工
+		do_stuff(int sockfd, MSG *pbuf);
+		break;
+		
+		case ADMIN://代表管理员
+		do_admin(int sockfd, MSG *pbuf);
+>>>>>>> fc9cad3396246ffc08aa7922d0ecdc11da9b3858
 		break;						
 	}
 	close(sockfd);
--- G3_2/server.c
+++ G3_2/server.c
@@ -6,12 +6,21 @@ void do_login(int socked, MSG *pbuf)
 {
     char *errmsg;
     char **resultp;
+<<<<<<< HEAD
 	
+=======
+>>>>>>> fc9cad3396246ffc08aa7922d0ecdc11da9b3858
     char cmd[256];
 
     int n_row, n_cloum;
 
+<<<<<<< HEAD
     sprintf(cmd, "select * from usr where name='%s'",pbuf->info.name);
+=======
+    sprintf(cmd, "select * from usr where name='%s'",pbuf->name);
+
+    puts(cmd);
+>>>>>>> fc9cad3396246ffc08aa7922d0ecdc11da9b3858
 
     if (sqlite3_get_table(db, cmd, &resultp, &n_row, &n_cloum, &errmsg) != SQLITE_OK)
     {
@@ -20,6 +29,7 @@ void do_login(int socked, MSG *pbuf)
     }
 
     if(n_row == 0){
+<<<<<<< HEAD
 		printf("-------------------------------------------------\n");
         strcpy(pbuf->data, "username or password err");
 		pbuf->ret=1;
@@ -37,6 +47,11 @@ void do_login(int socked, MSG *pbuf)
 			strcpy(pbuf->data, "登入err");
 			pbuf->ret=1;
 		}
+=======
+        strcpy(pbuf->data, "username or password err");
+    }else{
+        strcpy(pbuf->data, "ok");
+>>>>>>> fc9cad3396246ffc08aa7922d0ecdc11da9b3858
     }
 
     send(socked, pbuf, sizeof(MSG), 0);
@@ -46,21 +61,37 @@ void do_staff_change_passwd(int sockfd, MSG *pbuf)
 {
 	char *errmsg;
 	char cmd[256];
+<<<<<<< HEAD
 	sprintf(cmd,"update usr set passwd = \"%s\" where name = \"%s\"",pbuf->passwd,pbuf->info.name);
+=======
+	sprintf(cmd,"update data set passwd = '%s' where name = '%s'",pbuf->passwd,pbuf->name);
+>>>>>>> fc9cad3396246ffc08aa7922d0ecdc11da9b3858
 	// 根据 name 修改数据库内容为 passwd
     if( sqlite3_exec(db,cmd,NULL,NULL,&errmsg) == SQLITE_OK)
 	{
 		//修改成功 ret = 0，data = “修改密码完成”
+<<<<<<< HEAD
 		pbuf->ret = 0;
+=======
+		pbuf->ret = -1;
+>>>>>>> fc9cad3396246ffc08aa7922d0ecdc11da9b3858
 		strcpy(pbuf->data,"修改密码完成");
 	}
 	else 
 	{
+<<<<<<< HEAD
 		//查询失败 ret = 1, data = "修改密码失败“
 		pbuf->ret = 1;
 		strcpy(pbuf->data,"修改密码失败");
 	}
 	send(sockfd,pbuf,sizeof(MSG),0);
+=======
+		//查询失败 ret = -1, data = "修改密码失败“
+		pbuf->ret = 0;
+		strcpy(pbuf->data,"修改密码失败");
+	}
+	send(sockfd,(void *)&pbuf,sizeof(MSG),0);
+>>>>>>> fc9cad3396246ffc08aa7922d0ecdc11da9b3858
 }
 
 void do_staff_query(int sockfd, MSG *pbuf)
@@ -70,12 +101,17 @@ void do_staff_query(int sockfd, MSG *pbuf)
 	char **user_msg;
 	char cmd[256];
 	int  n_row,n_cloum;
+<<<<<<< HEAD
 	sprintf(cmd,"select * from usr where name = \"%s\"",pbuf->info.name);
+=======
+	sprintf(cmd,"select from data where name = %s",pbuf->name);
+>>>>>>> fc9cad3396246ffc08aa7922d0ecdc11da9b3858
 
     if( sqlite3_get_table(db,cmd,&user_msg,&n_row,&n_cloum,&errmsg) == SQLITE_OK)
 	{
 		//查询成功 ret = 0，查询到的信息保存在user_msg中
 		//数据库顺序： name,passwd,id,trueName,addr,age,salary,level 
+<<<<<<< HEAD
 		pbuf->ret = 0;
 		strcpy(pbuf->data,"获得员工信息成功");
 		pbuf->info.id = atoi(user_msg[9]);
@@ -92,15 +128,46 @@ void do_staff_query(int sockfd, MSG *pbuf)
 	}
     //发送 info
 	send(sockfd,pbuf,sizeof(MSG),0);
+=======
+		pbuf->ret = -1;
+		strcpy(pbuf->data,"获得员工信息成功");
+		pbuf->info.id = atoi(user_msg[10]);
+		strcpy(pbuf->info.name,user_msg[11]);
+		strcpy(pbuf->info.addr,user_msg[12]);
+		pbuf->info.age = atoi(user_msg[13]);
+		pbuf->info.salary = atof(user_msg[14]);
+		pbuf->ret = atoi(user_msg[15]);
+	}
+	else 
+	{
+		//查询失败 ret = -1, data = "获得员工信息失败“
+		pbuf->ret = 0;
+		strcpy(pbuf->data,"获得员工信息成失败");
+	}
+    //发送 info
+	send(sockfd,(void *)&pbuf,sizeof(MSG),0);
+>>>>>>> fc9cad3396246ffc08aa7922d0ecdc11da9b3858
 }
 
 void do_admin_query(int sockfd, MSG *pbuf)
 {
+<<<<<<< HEAD
 
+=======
+    // 若 data为空 发送当前用户信息
+	// 若 data不为空 查询用户并将此用户的信息发送
+	if(strcmp(pbuf->data,"") == 0)
+	{
+		do_staff_query(sockfd,pbuf);
+	}
+	else
+	{
+>>>>>>> fc9cad3396246ffc08aa7922d0ecdc11da9b3858
 	    char *errmsg;
 	    char cmd[256];
 	    char **user_msg;
 	    int  nrow,ncolumn;
+<<<<<<< HEAD
 		//通过比较名字得到内容
 	    sprintf(cmd,"select * from usr where name = \"%s\"",pbuf->info.name);
 
@@ -125,6 +192,87 @@ void do_admin_query(int sockfd, MSG *pbuf)
         //发送 info
 	    send(sockfd,pbuf,sizeof(MSG),0);
     
+=======
+	    sprintf(cmd,"select from data where name = %s",pbuf->data);
+
+        if( sqlite3_get_table(db,cmd,&user_msg,&nrow,&ncolumn,&errmsg) != SQLITE_OK)
+  	    {
+		    //查询成功 ret = 0，查询到的信息保存在user_msg中
+		    //数据库顺序： name,passwd,id,trueName,addr,age,salary,level 
+		    pbuf->ret = -1;
+		    strcpy(pbuf->data,"获得员工信息成功");
+		    pbuf->info.id = atoi(user_msg[10]);
+		    strcpy(pbuf->info.name,user_msg[11]);
+		    strcpy(pbuf->info.addr,user_msg[12]);
+		    pbuf->info.age = atoi(user_msg[13]);
+		    pbuf->info.salary = atof(user_msg[14]);
+		    pbuf->ret = atoi(user_msg[15]);
+	    }
+	    else 
+	    {
+		    //查询失败 ret = -1, data = "获得员工信息失败“
+		    pbuf->ret = 0;
+		    strcpy(pbuf->data,"获得员工信息成失败");
+	    }
+        //发送 info
+	    send(sockfd,(void *)&pbuf,sizeof(MSG),0);
+    }
+}
+
+void do_admin_add(int sockfd, MSG *pbuf)
+{
+	char *errmsg;
+	char cmd[256];
+	char **user_msg;
+	int nrow = 0,ncolumn = 0;
+	sprintf(cmd,"select from data where name = %s",pbuf->data);
+    sqlite3_get_table(db,cmd,&user_msg,&nrow,&ncolumn,&errmsg);
+  	if(nrow !=0 || ncolumn != 0)
+	{
+		pbuf->ret = -1;
+		strcpy(pbuf->data,"此用户名已被注册");
+	}
+	else
+	{
+	    sprintf(cmd,"insert into data values('%s','%s',%d,'%s',\
+					 '%s',%d,%f,%d",pbuf->data,pbuf->passwd,pbuf->info.id,\
+					 pbuf->info.name,pbuf->info.addr,pbuf->info.age,\
+					 pbuf->info.salary,pbuf->ret);
+		if(sqlite3_exec(db,cmd,NULL,NULL,&errmsg) == SQLITE_OK)
+		{
+			pbuf->ret = 0;
+			strcpy(pbuf->data,"添加成功");
+		}else
+		{
+		    pbuf->ret = -1;
+			strcpy(pbuf->data,"添加失败");
+		};
+	}
+	send(sockfd,(void *)&pbuf,sizeof(MSG),0);
+}
+
+void do_admin_del(int sockfd, MSG *pbuf)
+{
+	//查询data用户 从数据库删除
+	char *errmsg;
+	char cmd[256];
+	sprintf(cmd,"delete from data where name = '%s'",pbuf->data);
+	//数据库删除操作
+	if( sqlite3_exec(db,cmd,NULL,NULL,&errmsg) == SQLITE_OK)
+	{
+		//删除成功 ret = 0，data = “删除成功”
+		pbuf->ret = 0;
+		strcpy(pbuf->data,"删除成功");
+	}
+	else 
+	{
+		//删除失败 ret = -1, data = "删除失败“
+		pbuf->ret = 0;
+		strcpy(pbuf->data,"登陆成功");
+	}
+	//将登录的结果反馈给用户端
+	send(sockfd,(void *)&pbuf,sizeof(MSG),0);
+>>>>>>> fc9cad3396246ffc08aa7922d0ecdc11da9b3858
 }
 
 void do_admin_add(int sockfd, MSG *pbuf)
@@ -211,6 +359,7 @@ void *handler(void * arg)
                 do_admin_query(connfd, &msg);
                 break;
             case ADMIN_DEL_STAFF:
+<<<<<<< HEAD
 				printf("进入减少前");
               do_admin_del(connfd, &msg);
                 break;
@@ -219,6 +368,12 @@ void *handler(void * arg)
 				printf("进入add前");
               do_admin_add(connfd, &msg);
 			  printf("进入del后");
+=======
+              do_admin_del(connfd, &msg);
+                break;
+            case ADMIN_ADD_STAFF:
+              do_admin_add(connfd, &msg);
+>>>>>>> fc9cad3396246ffc08aa7922d0ecdc11da9b3858
                 break;
             default:
                 break;
@@ -239,9 +394,16 @@ void data_base_init()
     int no;
     char name[64];
 
+<<<<<<< HEAD
 	char sql[256] = {"create table usr(level int,passwd text,id int,name text,addr text ,age int,salary float);"};
 	sqlite3_exec(db,sql,NULL,NULL,&errmsg);
 	sqlite3_exec(db,"insert into usr values(101,'bat',1000,'Tony','济南',18,8000);",NULL,NULL,&errmsg);
+=======
+	char sql[256] = {"create table usr(level int,passwd text,id int,name text,addr text \
+					 age int,salary float)"};
+	sqlite3_exec(db,sql,NULL,NULL,&errmsg);
+	sqlite3_exec(db,"insert into data values(101,'bat',1000,'Tony','济南',18,8000);",NULL,NULL,&errmsg);
+>>>>>>> fc9cad3396246ffc08aa7922d0ecdc11da9b3858
 	
 }
 
@@ -285,6 +447,7 @@ int main(int argc, char *argv[])
     {
         perror("fail to listen");
         exit(-1);
+<<<<<<< HEAD
 	}
 	//接受客户端请求，创建子线程
 	while(1){
@@ -300,4 +463,21 @@ int main(int argc, char *argv[])
 	}
 	//pthread_join(tid,NULL);
 	//	pthread_join(tid, &ret);
+=======
+    }
+   //接受客户端请求，创建子线程
+    
+        if((connfd = accept(listenfd, NULL, NULL)) < 0){
+            perror("fail to accept");
+            exit(-1);
+        }
+    if(pthread_create(&tid, NULL, handler, &connfd) != 0)
+	{
+		perror("fail to create");
+		exit(1);
+	}
+	//pthread_join(tid,NULL);
+	pthread_join(tid, &ret);
+	
+>>>>>>> fc9cad3396246ffc08aa7922d0ecdc11da9b3858
 }
